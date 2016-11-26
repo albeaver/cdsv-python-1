dddc545604ba92d180fec9b334638c953d133b0b
albeaver <albeaver@users.noreply.github.com>
2016-11-26 09:20:07 -0800

    Created contact playground Nov 26. 2016
    Works in Python 3.5

---
+++ contactplaygroundfinal.py
@@ -0,0 +1,21 @@
+#Hello. This is the final code for when we made a 'contact card' for you.
+myName = "Connor"
+myAddress = "1 A St"
+myCity = "Mountain View"
+myZipCode = "11111"
+#myAge is an Int
+myAge = 13
+#isFriend is a Boolean
+isFriend = True
+#Print a 'Contact Card'
+print("---------------------------")
+print("Name: ")
+print(myName)
+print("Address: ")
+print(myAddress)
+print(myCity)
+print(myZipCode)
+print("Age: ")
+print(myAge)
+print("Is friend: ")
+print(isFriend)
